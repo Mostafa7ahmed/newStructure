424699d9b0c3137bfc6af21deb63efd41aa74abd
mostafa hamed <Mostafa7ahmed>
2025-07-12 23:32:52 +0300

    Hero

--- src/app/Layout/navbar/navbar.component.ts
+++ src/app/Layout/navbar/navbar.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [RouterLink,CommonModule ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  isScrolled = false;
 
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.isScrolled = window.scrollY > 50;
+  }
 }
